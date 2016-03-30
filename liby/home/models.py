275956367b82bed438589af4695407035210b0e2
Mateus Rocha <mateusrochagoncalves@gmail.com>
2016-03-30 10:28:11 -0300

    Criacao classe usuario

--- liby/home/models.py
+++ liby/home/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Models):
+	usuario = models.CharField(max_length=20)
+	senha = models.CharField(max_length=50)
+	email = models.EmailField()
+	cidade = models.CharField(max_length=50)
+	estado = models.CharField(max_length=2)
+	foto = models.ImageField()
