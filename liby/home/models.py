6edc2339fa05699b065116b17192a6a7a7b87e92
Aluno <aluno@decom-OptiPlex-780>
2016-04-06 09:47:03 -0300

    Adicionei classe Mensagem

--- liby/home/models.py
+++ liby/home/models.py
@@ -9,22 +9,30 @@ class Usuario(models.Models):
 	email = models.EmailField()
 	cidade = models.CharField(max_length=50)
 	estado = models.CharField(max_length=2)
-	foto = models.ImageField()
+	foto = models.ImageField()	
 
 class Livro(models.Models):
 	ISBN = models.CharField(max_length=20)
 	titulo = models.CharField(max_length=20)
 	autor = models.CharField(max_length=20)
 	edicao = models.CharField(max_length=20)
+	dono = models.Foreign(Usuario)
 
 	NOVO = '3'
 	BOM = '2'
 	RUIM = '1'
 
-	 ESTADO_OPCOES = (
-	 	(NOVO,'novo'),
-	 	(BOM,'bom'),
-	 	(RUIM,'ruim')
-	 )
+	ESTADO_OPCOES = (
+		(NOVO,'novo'),
+		(BOM,'bom'),
+		(RUIM,'ruim')
+	)
 
-	 estado = models.CharField(max_length=1,opcoes=ESTADO_OPCOES, default=BOM)
+	estado = models.CharField(max_length=1,opcoes=ESTADO_OPCOES, default=BOM)
+
+class Mensagem(models.Models):
+    data = models.DateField(auto_now_add=True)
+    conteudo = models.TextField()
+    destinatario = models.Foreign(Usuario)	 
+    remetente = models.Foreign(Usuario)
+    transacao = models.Foreign(Transacao)
