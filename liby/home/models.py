cc0bb63f9e5c943a015723f9d483ecd70ca896fd
Saulo Antunes <sauloantuness@gmail.com>
2016-04-06 10:15:34 -0300

    fix models

--- liby/home/models.py
+++ liby/home/models.py
@@ -3,23 +3,23 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Usuario(models.Models):
+class Usuario(models.Model):
 	usuario = models.CharField(max_length=20)
 	senha = models.CharField(max_length=50)
 	email = models.EmailField()
 	cidade = models.CharField(max_length=50)
 	estado = models.CharField(max_length=2)
-	foto = models.ImageField()
+	foto = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.usuario
 
-class Livro(models.Models):
+class Livro(models.Model):
 	ISBN = models.CharField(max_length=20)
 	titulo = models.CharField(max_length=20)
 	autor = models.CharField(max_length=20)
 	edicao = models.CharField(max_length=20)
-	dono = models.Foreign(Usuario)
+	dono = models.ForeignKey(Usuario)
 
 	NOVO = '3'
 	BOM = '2'
@@ -31,21 +31,10 @@ class Livro(models.Models):
 		(RUIM,'ruim')
 	)
 
-	estado = models.CharField(max_length=1,opcoes=ESTADO_OPCOES, default=BOM)
+	estado = models.CharField(max_length=1, choices=ESTADO_OPCOES, default=BOM)
 
 	def __str__(self):
 		return self.titulo
-
-
-class Mensagem(models.Models):
-    data = models.DateField(auto_now_add=True)
-    conteudo = models.TextField()
-    destinatario = models.Foreign(Usuario)	 
-    remetente = models.Foreign(Usuario)
-    transacao = models.Foreign(Transacao)
-
-	def __str__(self):
-		return self.conteudo
 
 class Transacao(models.Model):
 	livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
@@ -55,3 +44,13 @@ class Transacao(models.Model):
 	avaliacaoSolicitante = models.TextField()
 	notaDono = models.IntegerField()
 	notaSolicitante = models.IntegerField()
+
+class Mensagem(models.Model):
+	data = models.DateField(auto_now_add=True)
+	conteudo = models.TextField()
+	destinatario = models.ForeignKey(Usuario, related_name='destinatario')
+	remetente = models.ForeignKey(Usuario, related_name='remetente')
+	transacao = models.ForeignKey(Transacao)
+
+	def __str__(self):
+		return self.conteudo
