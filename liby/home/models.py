a170adf8830039378d93b1561b36551f10a53db9
Lucas <lucasramonsa@gmail.com>
2016-04-25 19:08:26 -0300

    Página Home

--- liby/home/models.py
+++ liby/home/models.py
@@ -11,46 +11,45 @@ class Perfil(models.Model):
 		return self.nome
 
 class Livro(models.Model):
+	ISBN = models.CharField(max_length=20)
 	titulo = models.CharField(max_length=100)
-	autor  = models.CharField(max_length=100)
-	capa   = models.CharField(max_length=100)
+	autor = models.CharField(max_length=100)
+	capa  = models.CharField(max_length=100)
+	edicao = models.CharField(max_length=20)
+	data = models.DateField(auto_now_add=True)
 	descricao = models.TextField()
-	dono = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+	dono = models.ForeignKey(Perfil)	
+
+	NOVO = '3'
+	BOM = '2'
+	RUIM = '1'
+
+	ESTADO_OPCOES = (
+ 		(NOVO,'novo'),
+ 		(BOM,'bom'),
+ 		(RUIM,'ruim')
+ 	)
+
+	estado = models.CharField(max_length=1, choices=ESTADO_OPCOES, default=BOM)
 
 	def __str__(self):
 		return self.titulo + ' by ' + self.autor
 
-# 	ISBN = models.CharField(max_length=20)
-# 	edicao = models.CharField(max_length=20)
+class Transacao(models.Model):
+	livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+	usuarioSolicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+	concluida = models.BooleanField(default=False)
+	avaliacaoDono = models.TextField()
+	avaliacaoSolicitante = models.TextField()
+	notaDono = models.IntegerField()
+	notaSolicitante = models.IntegerField()
 
-# 	NOVO = '3'
-# 	BOM = '2'
-# 	RUIM = '1'
+class Mensagem(models.Model):
+	data = models.DateField(auto_now_add=True)
+	conteudo = models.TextField()
+	destinatario = models.ForeignKey(Perfil, related_name='destinatario')
+	remetente = models.ForeignKey(Perfil, related_name='remetente')
+	transacao = models.ForeignKey(Transacao)
 
-# 	ESTADO_OPCOES = (
-# 		(NOVO,'novo'),
-# 		(BOM,'bom'),
-# 		(RUIM,'ruim')
-# 	)
-
-# 	estado = models.CharField(max_length=1, choices=ESTADO_OPCOES, default=BOM)
-
-
-# class Transacao(models.Model):
-# 	livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
-# 	usuarioSolicitante = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-# 	concluida = models.BooleanField(default=False)
-# 	avaliacaoDono = models.TextField()
-# 	avaliacaoSolicitante = models.TextField()
-# 	notaDono = models.IntegerField()
-# 	notaSolicitante = models.IntegerField()
-
-# class Mensagem(models.Model):
-# 	data = models.DateField(auto_now_add=True)
-# 	conteudo = models.TextField()
-# 	destinatario = models.ForeignKey(Usuario, related_name='destinatario')
-# 	remetente = models.ForeignKey(Usuario, related_name='remetente')
-# 	transacao = models.ForeignKey(Transacao)
-
-# 	def __str__(self):
-# 		return self.conteudo
+	def __str__(self):
+		return self.conteudo
--- liby/home/views.py
+++ liby/home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from home.models import Livro
 
 def login(request):
 	return render(request, 'home/login.html')
@@ -14,3 +15,13 @@ def logout(request):
 @login_required
 def home(request):
 	return render(request, 'home/home.html')
+
+def books(request):
+	a = len(Livro.objects.all())
+	b = 0
+	if a > 10:
+		b = a - 10
+	context = {
+		'livros' : Livro.objects.all()[b::]
+	}
+	return render(request, 'home/home.html', context)
