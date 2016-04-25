01b757830f27514545225c80b691344491a192dd
Lucas Teodoro <theodoroh@hotmail.com>
2016-04-25 13:02:50 -0300

    Adicionar livro

--- liby/livros/views.py
+++ liby/livros/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from home.models import Livro
 
 @login_required
 def livros(request):
@@ -13,4 +14,17 @@ def meusLivros(request):
 
 @login_required
 def adicionar(request):
+
+	if request.method == 'POST':
+		l = Livro()
+		l.titulo = request.POST['titulo']
+		l.autor = request.POST['autor']
+		l.capa = request.POST['capa']
+		l.descricao = request.POST['descricao']
+		l.dono = request.user.perfil
+
+		l.save()
+
+		return redirect('/livros/meus-livros/')
+
 	return render(request, 'livros/adicionar.html')
