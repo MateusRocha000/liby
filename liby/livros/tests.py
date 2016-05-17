ac5579542c8d7ce8aa7028faba8aa54d3810ac8f
Lucas <lucasramonsa@gmail.com>
2016-05-17 18:55:40 -0300

    Teste unitário para busca por ISBN

--- liby/livros/tests.py
+++ liby/livros/tests.py
@@ -8,7 +8,7 @@ class LivrosMethodTests(TestCase):
     def setUp(self):
         self.u = User.objects.create(username='teste1')
         self.p = Perfil.objects.create(nome="Saulo", user=self.u)
-        self.l = Livro.objects.create(titulo="A menina que roubava livros", dono=self.p)
+        self.l = Livro.objects.create(titulo="A menina que roubava livros", ISBN='9788598078373', dono=self.p)
 
     def test_meus_livros(self):
         self.assertIn(self.l, self.p.meusLivros())
@@ -17,6 +17,10 @@ class LivrosMethodTests(TestCase):
     def test_pesquisar_livro(self):
     	self.assertIn(self.l, buscarLivro('A menina'))
     	self.assertNotIn(self.l, buscarLivro('outrolivro'))
+
+    def test_pesquisar_isbn(self):
+        self.assertIn(self.l, buscarLivro('9788598078373'))
+        self.assertNotIn(self.l, buscarLivro('000001'))
 
     def test_adicionar_livro(self):
     	l = adicionarLivro(dono=self.p, titulo="O guia do mochileiro", autor="Douglas Adams")
